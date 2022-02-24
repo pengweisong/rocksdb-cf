ddfa8d77cb4e51e49e0e02cffc06d0c3036a02c9
Cloud User <kai.liu@kai.liu-165>
2022-02-24 04:07:25 +0000

    fixed

--- drop_column_family_test.cpp
+++ drop_column_family_test.cpp
@@ -13,7 +13,7 @@
 #include "rocksdb/options.h"
 #include "rocksdb/slice.h"
 #include "rocksdb/utilities/checkpoint.h"
-std::string kDBPath = "/tmp/column_family_test";
+std::string kDBPath = "/tmp/drop_column_family_test";
 using ROCKSDB_NAMESPACE::Checkpoint;
 using ROCKSDB_NAMESPACE::ColumnFamilyDescriptor;
 using ROCKSDB_NAMESPACE::ColumnFamilyHandle;
@@ -22,6 +22,7 @@ using ROCKSDB_NAMESPACE::DB;
 using ROCKSDB_NAMESPACE::DBOptions;
 using ROCKSDB_NAMESPACE::DbPath;
 using ROCKSDB_NAMESPACE::ExportImportFilesMetaData;
+using ROCKSDB_NAMESPACE::FlushOptions;
 using ROCKSDB_NAMESPACE::ImportColumnFamilyOptions;
 using ROCKSDB_NAMESPACE::kDefaultColumnFamilyName;
 using ROCKSDB_NAMESPACE::Options;
@@ -35,73 +36,61 @@ int main()
 {
     // open DB
     Options options;
-
     options.create_if_missing = true;
-    DB *db;
-    Status s = DB::Open(options, kDBPath, &db);
-    assert(s.ok());
-
-    // create column family
-    ColumnFamilyHandle *cf1 = nullptr, *cf2 = nullptr;
-
-    ColumnFamilyOptions cf1_options, cf2_options;
-    cf1_options.write_buffer_size = 80920;
-    cf2_options.write_buffer_size = 80920;
-
+    DB *db = nullptr;
     DbPath path1, path2;
     path1.path = "/tmp/sst1";
     path2.path = "/tmp/sst2";
-    vector<DbPath> paths_{path1};
-    cf1_options.cf_paths = paths_;
-    paths_.clear();
-    paths_.emplace_back(path2);
-    cf2_options.cf_paths = paths_;
+
+    ColumnFamilyHandle *cf1 = nullptr, *cf2 = nullptr;
+    ColumnFamilyOptions cf1_options, cf2_options;
+    cf1_options.write_buffer_size = 80920;
+    cf2_options.write_buffer_size = 80920;
+    cf1_options.cf_paths = {path1};
+    cf2_options.cf_paths = {path2};
+
+    Status s = DB::Open(options, kDBPath, &db);
+    assert(s.ok());
+
     s = db->CreateColumnFamily(cf1_options, "cf_one", &cf1);
     assert(s.ok());
     s = db->CreateColumnFamily(cf2_options, "cf_two", &cf2);
     assert(s.ok());
 
-    s = db->DestroyColumnFamilyHandle(cf1);
-    assert(s.ok());
-    s = db->DestroyColumnFamilyHandle(cf2);
-    assert(s.ok());
-    delete db;
-
-    std::vector<ColumnFamilyDescriptor> column_families;
-
-    //必须打开所有已经存在的列族
-    column_families.push_back(ColumnFamilyDescriptor(kDefaultColumnFamilyName,
-                                                     ColumnFamilyOptions()));
-
-    column_families.push_back(ColumnFamilyDescriptor("cf_one", cf1_options));
-    column_families.push_back(ColumnFamilyDescriptor("cf_two", cf2_options));
-    std::vector<ColumnFamilyHandle *> handles;
-    s = DB::Open(DBOptions(), kDBPath, column_families, &handles, &db);
-
-    assert(s.ok());
-
-    for (int i = 0; i < 10000; ++i)
-        s = db->Put(WriteOptions(), handles[1], Slice("key1"), Slice("value1"));
-    assert(s.ok());
-    for (int i = 0; i < 10000; ++i)
-        s = db->Put(WriteOptions(), handles[2], Slice("key2"), Slice("value2"));
+    for (int i = 0; i < 10000; ++i) {
+        s = db->Put(WriteOptions(), cf1, Slice("key1"), Slice("value1"));
+        assert(s.ok());
+    }
+    string put_value = "value";
+    string key;
+    for (int i = 0; i < 26; ++i) {
+        char temp = static_cast<char>('a' + i);
+        key.push_back(temp);
+        s = db->Put(WriteOptions(), cf2, key, put_value);
+        assert(s.ok());
+        key.clear();
+    }
 
     string value;
-    assert(db->Get(ReadOptions(), handles[1], "key1", &value).ok());
+    assert(db->Get(ReadOptions(), cf1, "key1", &value).ok());
     assert(value == "value1");
     value.clear();
-    assert(db->Get(ReadOptions(), handles[2], "key2", &value).ok());
-    assert(value == "value2");
+    for (int i = 0; i < 26; ++i) {
+        char temp = static_cast<char>('a' + i);
+        string key;
+        key.push_back(temp);
+        s = db->Get(ReadOptions(), cf2, key, &value);
+        assert(s.ok());
+        assert(value == put_value);
+        value.clear();
+    }
 
-    //删除/tmp/sst1/下所有sst文件
+    //删除cf1对应的sst文件不删除cf2对应的sst文件
+    assert(db->DropColumnFamily(cf1).ok());
+    assert(db->DestroyColumnFamilyHandle(cf1).ok());
+    assert(db->Flush(FlushOptions(), cf2).ok());
+    assert(db->DestroyColumnFamilyHandle(cf2).ok());
 
-    assert(db->DropColumnFamily(handles[1]).ok());
-    assert(db->DestroyColumnFamilyHandle(handles[1]).ok());
-    while (1)
-        ;
-    // close db
-    assert(db->DropColumnFamily(handles[2]).ok());
-    assert(db->DestroyColumnFamilyHandle(handles[2]).ok());
     delete db;
 
     return 0;
