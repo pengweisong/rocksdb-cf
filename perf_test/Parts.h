0e987cf93f2e7f57d0858dd79c782106368a5461
baiqiubai <81951368+baiqiubai@users.noreply.github.com>
2022-04-01 17:53:59 +0800

    fix bug

---
+++ perf_test/Parts.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cstdint>
+#include <iostream>
+
+#include "KVEngine.h"
+#include "Keys.h"
+#include "Options.h"
+
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+
+using PartId = int32_t;
+
+class Part {
+ public:
+  Part(PartId id, KVEngine* engine);
+
+  ~Part();
+
+  KVEngine* getEngine();
+
+  void addVertex(const VertexKey& v, const rocksdb::Slice& value);
+
+  void removeVertex(const VertexKey& v);
+
+  void getVertex(const VertexKey& k, std::string* value);
+
+  void addEdge(const EdgeKey& e, const rocksdb::Slice& value);
+
+  void removeEdge(const EdgeKey& e);
+
+  void getEdge(const EdgeKey& e, std::string* value);
+
+  void getNeighbor(const VertexKey& v, std::vector<EdgeKey>* edges, bool isInEdge = true);
+
+  rocksdb::Iterator* newIterator();
+
+ private:
+  void getEdges(const VertexKey& v,
+                const std::string& key,
+                std::vector<EdgeKey>* edges,
+                bool isInEdge);
+
+  KVEngine* engine_;
+  PartId id_;
+};
